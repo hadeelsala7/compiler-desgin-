0741699fbf378024e5293168fc13aefbb16b260b
Hadeel Salah <h.salah.technical@gmail.com>
2022-05-28 19:46:15 +0300

    Create Token.java

---
+++ src/project/Token.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+public class Token {
+    String Name;
+    String Type;
+    int LineNo;
+    
+    public Token(String n , String t , int ln){
+        Name = n;
+        Type = t;
+        LineNo = ln;
+    }
+}
+
