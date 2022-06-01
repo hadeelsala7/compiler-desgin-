17da5a1020de493300819cbc501c00a98fef7307
Hadeel Salah <h.salah.technical@gmail.com>
2022-06-01 21:35:56 +0300

    Create Token.java

---
+++ Token.java
@@ -0,0 +1,17 @@
+
+// هديل صلاح//// 20181473 
+//سارة شحادة 20180209
+//أصالةالقيق /// 20183151
+//بيسان أبو جندي /// 20193053 
+
+public class Token {
+    String Name;
+    String Type;
+    int LineNo;
+
+    public Token(String n, String t, int ln) {
+        Name = n;
+        Type = t;
+        LineNo = ln;
+    }
+}
